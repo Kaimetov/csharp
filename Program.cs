b0257c30ae8f3bd5f5696f4e4168be84f26e2365
Kairat <kairat.telektesovich@gmail.com>
2024-01-07 20:40:31 +0600

    First version

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Project;
+
+class Program
+{    
+    static void Main(string[] args)
+    {
+        int [] nums = {5,7,4,30,45,-3,-2,57,9};
+        nums = nums.Where(mas => mas % 2 != 0 && mas >=0).ToArray();
+        foreach(int mas in nums)
+        Console.WriteLine(mas);   
+    }
+}

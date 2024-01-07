8f8ee02d2575699832367917bd4ed1bb935b70ee
Kairat <kairat.telektesovich@gmail.com>
2024-01-07 20:44:09 +0600

    зменили файл

--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ class Program
 {    
     static void Main(string[] args)
     {
-        int [] nums = {5,7,4,30,45,-3,-2,57,9};
-        nums = nums.Where(mas => mas % 2 != 0 && mas >=0).ToArray();
-        foreach(int mas in nums)
-        Console.WriteLine(mas);   
+        Console.WriteLine("Hello");
     }
 }
